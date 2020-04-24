781ddd90cae935c0fd40b3809818b2161c5e3bb6
Albert Hattingh <albert.h0504@gmail.com>
2020-04-24 11:10:08 +0200

    Resize main form.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1203, 710);
+            this.ClientSize = new System.Drawing.Size(1136, 601);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
